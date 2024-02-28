ca711015c12e006043ed788685d63caf0b194d72
Thiago Egon Lange <thiago_lange@yahoo.com.br>
2024-02-28 19:21:46 -0300

    adding args in the fileserver

--- mai.go
+++ mai.go
@@ -4,10 +4,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("/Users/devops/tmp"))
-	fmt.Println("subindo servidor na porta 9000...")
-	log.Fatal(http.ListenAndServe(":9000", fs))
+	if len(os.Args) != 3 {
+		fmt.Println("Uso: go run main.go <diretorio> <porta>")
+		os.Exit(1)
+	}
+	httpDir := os.Args[1]
+	porta := os.Args[2]
+	fs := http.FileServer(http.Dir(httpDir))
+	fmt.Printf("Subindo servidor na porta %s ...", porta)
+	log.Fatal(http.ListenAndServe(":"+porta, fs))
 }
